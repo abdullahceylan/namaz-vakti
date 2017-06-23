ed1a57b3f4d64162c640bbdf48909246ac144aca
Adem ilter <ademilter@gmail.com>
2017-06-24 00:13:39 +0300

    pwa -> webpack

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -1,5 +1,3 @@
-var fs = require('fs')
-var path = require('path')
 var utils = require('./utils')
 var webpack = require('webpack')
 var config = require('../config')
@@ -15,7 +13,7 @@ Object.keys(baseWebpackConfig.entry).forEach(function (name) {
 
 module.exports = merge(baseWebpackConfig, {
   module: {
-    rules: utils.styleLoaders({sourceMap: config.dev.cssSourceMap})
+    rules: utils.styleLoaders({ sourceMap: config.dev.cssSourceMap })
   },
   // cheap-module-eval-source-map is faster for development
   devtool: '#cheap-module-eval-source-map',
@@ -30,9 +28,7 @@ module.exports = merge(baseWebpackConfig, {
     new HtmlWebpackPlugin({
       filename: 'index.html',
       template: 'index.html',
-      inject: true,
-      serviceWorkerLoader: `<script>${fs.readFileSync(path.join(__dirname,
-        './service-worker-dev.js'), 'utf-8')}</script>`
+      inject: true
     }),
     new FriendlyErrorsPlugin()
   ]
